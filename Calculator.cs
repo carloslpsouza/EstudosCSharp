5fa2b23467663b93e33fcc66d6b4f485666cf4f3
Carlos Souza <carlos.souza@unigranrio.br>
2023-07-22 17:55:49 -0300

    Estudo callback

---
+++ Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace MeuProjeto;
+using System;
+public delegate void CallBackDelegate(int result);
+
+public class Calculator
+{
+  public void Sum(int a, int b, CallBackDelegate callBack)
+  {
+    int result = a + b;
+    callBack(result);
+  }
+  public void Multiply(int a, int b, CallBackDelegate callBack)
+  {
+    int result = a * b;
+    callBack(result);
+  }
+
+}
